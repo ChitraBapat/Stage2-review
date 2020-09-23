68bbc479555e4de43ae8d38a68dfab3d0030492d
Chitra Bapat <chibapat26@gmail.com>
2020-09-23 19:35:41 +0530

    changed

--- test.sh
+++ test.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 x=1
 c=0
+sum=0
+
 while [ $x -eq 1 ];
 do
 
 a=$((RANDOM%6 + 1))
 b=$((RANDOM%6 + 1))
 
-sum=$(( a+b ))
+sum=$(( $sum+$a+$b ))
 (( c++ ))
 echo "$c th number of time roll"
-echo "The result of both the unbaised roll is $a and $b respectively and the su>
-if [ $sum -eq 12 ];
+echo "The result of both the unbaised roll is $a and $b respectively and the cummulative sum is $sum"
+if [ $(( $a+$b )) -eq 12 ];
 then
 x=0
+echo "Both the dice show 6"
 fi
 done
